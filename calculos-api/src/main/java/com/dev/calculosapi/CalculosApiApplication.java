d90a53f28bee2fcd9e6fbe2875be3d82cf5d63a9
Thales R. Menezes <thalesmenezes286@gmail.com>
2025-01-07 14:36:25 -0300

    Commit Inicial

---
+++ calculos-api/src/main/java/com/dev/calculosapi/CalculosApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.calculosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculosApiApplication.class, args);
+    }
+
+}
---
+++ calculos-api/src/test/java/com/dev/calculosapi/CalculosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.calculosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
